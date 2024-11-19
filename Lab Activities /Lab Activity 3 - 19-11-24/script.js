9de9be60374a8168e9ea04e2689325bb259b1b69
princessdlssnts <134253666+princessdlssnts@users.noreply.github.com>
2024-11-19 14:12:36 +0800

    Create script.js

---
+++ Lab Activities /Lab Activity 3 - 19-11-24/script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const textInput = document.getElementById('textInput');
+    const wordCount = document.getElementById('wordcount');
+    const sentenceCount = document.getElementById('sentenceCount');
+  
+    textInput.addEventListener('input', () => {
+      const text = textInput.value.trim();
+  
+      // Word Count
+      const words = text.split(/\s+/).filter(word => word.length > 0);
+      wordCount.textContent = `Words: ${words.length}`;
+  
+      // Sentence Count
+      const sentences = text.split(/[.!?]+\s/).filter(sentence => sentence.trim().length > 0);
+      sentenceCount.textContent = `Sentences: ${sentences.length}`;
+    });
+  });
